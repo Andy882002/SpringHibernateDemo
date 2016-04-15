1cbc60d950fe69756c839dc03e082b1deb8c15fa
Anda <Anda@VAIO-VGN-FW560F>
2016-04-15 12:26:24 -0700

    This is my testing githut demo

---
+++ TestGITDemo/src/demo/test/MainTest.java
@@ -0,0 +1,14 @@
+package demo.test;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp=new Employee();
+		emp.setId(1000);
+		emp.setName("Steven Oliver");
+		System.out.println(emp.toString());
+
+	}
+
+}
