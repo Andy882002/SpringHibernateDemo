0395ee9c4ad7bfb75e73270fc9a05ed9a7575f2f
Anda <Anda@192.168.1.146>
2016-04-15 14:15:27 -0700

    This is update version2

--- TestGITDemo/src/demo/test/MainTest.java
+++ TestGITDemo/src/demo/test/MainTest.java
@@ -5,9 +5,15 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Employee emp=new Employee();
+		Employee emp2=new Employee();
 		emp.setId(1000);
 		emp.setName("Steven Oliver");
+		emp2.setId(1001);
+		emp2.setName("Julie Amander");
 		System.out.println(emp.toString());
+		
+		System.out.printf("%s", emp.equals(emp2));
+		
 
 	}
 
